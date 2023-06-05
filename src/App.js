89cc423899e48d7fde1b32a4abbbd2649fbc18d9
gamerfoxx <admin@josephludbrook.com>
2023-06-05 18:46:01 -0400

    add loading state

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler(){
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
-    
+
     const transformedMovies = data.results.map(movieData =>{
       return {
         id: movieData.episode_id,
@@ -20,6 +22,7 @@ function App() {
       }
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -28,7 +31,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No movies available</p>}
+        {isLoading && <p>Is loading...</p>}
       </section>
     </React.Fragment>
   );
